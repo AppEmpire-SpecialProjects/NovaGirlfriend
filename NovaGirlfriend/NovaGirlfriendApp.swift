4a1c2a8cca88a714dceb0caab9b86a5c2ae3c78c
Zaven <adamyanzaven722@gmail.com>
2026-09-08 18:38:02 +0400

    Initial Commit

---
+++ NovaGirlfriend/NovaGirlfriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaGirlfriendApp.swift
+//  NovaGirlfriend
+//
+//  Created by Zaven Adamyan on 08.09.26.
+//
+
+import SwiftUI
+
+@main
+struct NovaGirlfriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
